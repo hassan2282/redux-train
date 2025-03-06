20ea10866be16241a5349da5375b42ed1126dba1
hassan2282 <taghavey.hassan@gmail.com>
2025-03-06 22:03:17 +0330

    add router to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Hello from './Hello';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-sky-300 text-sky-900 rounded-md p-2'>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ☻
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo animate-spin" alt="logo" />
+          <p className='bg-sky-300 text-sky-900 rounded-md p-2 selection:text-white'>
+            Edit <code>src/App.js</code> and save to reload
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Link to={'/home'}>
+              Learn React ☻            
+            </Link>
+          </a>
+        </header>
+      </div>
+      <Routes>
+        <Route path='/home' element={<Hello />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
